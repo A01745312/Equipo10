34620fad155d9018d40ee57a7525efd881bdc9ba
Paula Santoyo <a01745312@itesm.mx>
2022-11-11 23:26:43 -0600

    Modelo Aspiradora

--- M1. Actividad/m1_actividad.py
+++ M1. Actividad/m1_actividad.py
@@ -6,7 +6,7 @@
     Noviembre, 2022
 """
 from mesa import Agent, Model
-from mesa.space import SingleGrid
+from mesa.space import MultiGrid
 from mesa.time import SimultaneousActivation
 import numpy as np
 
@@ -15,13 +15,71 @@ class AgenteAspiradora(Agent):
     """
     Representa a un agente que va a limpiar la celda
     """
-    def __init__(self, unique_id, model):
+
+
+    def __init__(self, unique_id, model, pos):
         super().__init__(unique_id, model)
+        self.live = 3
+        self.next_state = None
+        self.pos = pos
+
+
+    def avanzar(self):
+        l = []
+        siguienteMovimiento = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center = True)
+        neighbors = self.model.grid.get_neighbors(self.pos,
+                                                moore=True,
+                                                include_center = True)
+        for n in neighbors:
+            if n.live == 1 and n.pos == self.pos:
+                n.live = 0
+            if n.live == 1:
+                l.append(n.pos)
+        if len(l) == 0:
+            posicion = self.random.choice(siguienteMovimiento)
+            self.model.grid.move_agent(posicion)
+        else:
+            posicion = self.random.choice(l)
+            self.model.grid.move_agent(posicion)
+    
+    
 
 
 class AgenteSuciedad(Agent):
     """
     Representa una celda sucia
     """
-    def __init__(self, unique_id: int, model: Model) -> None:
+
+
+    def __init__(self, unique_id: int, model: Model, pos) -> None:
         super().__init__(unique_id, model)
+        self.live = np.random.choice([0,1])
+        self.next_state = None
+        self.pos = pos
+
+class ModeloLimpieza(Model):
+    '''
+    Define el modelo de simulador de limpieza
+    '''
+    def __init__(self, width, height):
+        self.num_agents = width * height
+        self.grid = MultiGrid(width, height, True)
+        self.schedule = SimultaneousActivation(self)
+        self.running = True 
+
+        for (content, x, y) in self.grid.coord_iter():
+            if x == 1 and y == 1:
+                comienza = AgenteAspiradora(("A", 1, 1), self, (1,1))
+                self.grid.place_agent(comienza,(1,1))
+                self.schedule.add(comienza)
+            
+            dirty = AgenteSuciedad(("S", x, y), self, (x,y))
+            self.grid.place_agent(dirty,(x, y))
+            self.schedule.add(dirty)
+    
+    def step(self):
+        self.schedule.step()
+
--- M1. Actividad/m1_actividad_Viz.py
+++ M1. Actividad/m1_actividad_Viz.py
@@ -6,3 +6,31 @@
     Noviembre, 2022
 """
 from m1_actividad import *
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle",
+                 "Filled": "true",
+                 "Layer": 0,
+                 "Color": "black",
+                 "r": 0.5}
+
+    if agent.live == 1: #Rojos grandes cuando están vivos
+        portrayal["Color"] = "black"
+        portrayal["Layer"] = 0
+    elif agent.live == 3 :
+            portrayal["Color"] = "blue"
+            portrayal["Layer"] = 1
+
+    return portrayal
+ancho = 28
+alto = 28
+grid = CanvasGrid(agent_portrayal, ancho, alto, 750, 750)
+server = ModularServer(ModeloLimpieza,
+                       [grid],
+                       "Modelo limpieza",
+                       {"width":ancho, "height":alto})
+server.port = 8521 
+server.launch()
